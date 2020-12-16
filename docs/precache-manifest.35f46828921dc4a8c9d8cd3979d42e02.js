1b16ba8e7c0f34a69e1a758f565ea94ce83276cf
Sergio Herrera <sergioh24118642@gmail.com>
2020-12-16 13:28:13 -0400

    Actual buscador

---
+++ docs/precache-manifest.35f46828921dc4a8c9d8cd3979d42e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a72141a5df73753004ae5f6f21b5efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dc8e55fe90fdad79594",
+    "url": "/static/css/main.3e829833.chunk.css"
+  },
+  {
+    "revision": "094bb1bd5ba9dc5620b1",
+    "url": "/static/js/2.dcc60bab.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.dcc60bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc8e55fe90fdad79594",
+    "url": "/static/js/main.abdfa9de.chunk.js"
+  },
+  {
+    "revision": "8bf7f7832dff11fbba98",
+    "url": "/static/js/runtime-main.482171d5.js"
+  }
+]);
